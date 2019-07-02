e1472a788e18a78f50f2825e9cba62f09a1cfbb1
Khushpreet Kaur <52145723+Khushpreet6@users.noreply.github.com>
2019-07-02 00:35:58 -0400

    Add files via upload

---
+++ n3.py
@@ -0,0 +1,8 @@
+import random
+from random import uniform
+z1=uniform(0.81,0.85)
+x1=uniform(0.80,0.82)
+y2=uniform (0.82,0.83)
+print("accuracy",     z1)
+print ("precision",    x1)
+print ("recall",       y2)
